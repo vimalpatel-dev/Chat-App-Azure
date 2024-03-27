d4e398f3944e0bac1051d1e504a660069e58852a
vimalpatel123 <vimal.patel@technomark.io>
2024-03-27 12:08:12 +0530

    add db name to mongo connector

--- Server/Util/mongo_connection.js
+++ Server/Util/mongo_connection.js
@@ -4,10 +4,12 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     const mongoURL = process.env.MONGODB_URL;
+    const dbName = process.env.DBNAME || "notification";
 
     const conn = await mongoose.connect(`${mongoURL}`, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: dbName,
     });
     console.log(`MongoDB Connected at : ${conn.connection.host}`);
   } catch (error) {
